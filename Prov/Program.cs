d2ef36ba91cb00115f49ee4bc086f4db38c265bb
adamlinkanshapiro <71385867+adamlinkanshapiro@users.noreply.github.com>
2020-10-14 14:59:06 +0200

    prov

--- Prov/Program.cs
+++ Prov/Program.cs
@@ -6,7 +6,27 @@ namespace Prov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose any number between 1 and 30");
+
+            Console.WriteLine("1, 2, 3, 4, 5");
+
+            string userInput;
+            userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "1")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Miss");
+
+            }
+
+            else if (userInput == "5")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Hit");
+            }
         }
     }
 }
